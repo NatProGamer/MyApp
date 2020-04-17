7050a61fc638f5b2c042ab2a7f98e3401bfbf9a9
NatProGamer <33282651+NatProGamer@users.noreply.github.com>
2020-04-17 10:38:41 +0200

    New : Medium function

--- Numerics.py
+++ Numerics.py
@@ -57,3 +57,33 @@ def getmax(*numbers, **params):
         if list[i] > max:
             max = list[i]
     return max
+
+def getmedium(*numbers, **params):
+    ''' Get medium value
+    *numbers --> All the numbers
+    **params --> All the params
+        list --> A list of numbers
+            The others numbers would be ignored
+        start --> The research start index
+            The list must be specified
+        end --> The research end index
+            The list must be specified'''
+    if "list" in params:
+        list = params["list"]
+        start = 0
+        end = len(list)
+        if "start" in params:
+            start = params["start"]
+        if "end" in params:
+            end = params["end"] + 1
+    else:
+        list = []
+        for i in range(0, len(numbers)):
+            list.append(numbers[i])
+        start = 0
+        end = len(list)
+    sum = 0
+    for i in range(start, end):
+        sum += list[i]
+    medium = sum / (end - start)
+    return medium
